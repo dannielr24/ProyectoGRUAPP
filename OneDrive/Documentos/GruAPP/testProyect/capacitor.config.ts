0ccbf51862c6c4d688f6398e781e947b0f6e9cd8
dannielr24 <da.riosecoc@duocuc.cl>
2024-11-14 19:16:04 -0300

    Arreglos Nuevos

--- OneDrive/Documentos/GruAPP/testProyect/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'testProyect',
-  webDir: 'www'
-};
-
-export default config;
